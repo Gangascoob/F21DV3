1ce4df3dcc1784e47dce8cf31b02cd9783a6729e
Gangascoob <67718774+Gangascoob@users.noreply.github.com>
2022-03-12 16:27:35 +0000

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const selectedCountries = ["GBR", "ITA", "DEU", "NOR", "RUS", "LTU", "HUN"];
 			var projection = d3.geo.mercator() //use a standard projection to flatten the poles, see D3 projection plugin
 								   .center([ 13, 25 ]) //how to center the map, longitude, latitude
 								 //  .translate([50%, 50%]) center the resulting image in the svg
-								   .scale(67); // zoom, the smaller the value, the bigger the zoom
+								   .scale(100); // zoom, the smaller the value, the bigger the zoom
 			//Define path generator
 			var path = d3.geo.path()
 				   .projection(projection);
