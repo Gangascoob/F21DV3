2efb199f0c5261c3130b2b296d04c7f6ddb997f4
Gangascoob <67718774+Gangascoob@users.noreply.github.com>
2022-03-15 14:35:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
-const selectedCountries = ["GBR", "ITA", "DEU", "NOR", "RUS", "LTU", "HUN", "MDA"];
+const selectedCountries = ["GBR", "ITA", "DEU", "NOR", "RUS", "LTU", "HUN"];
 
-				
 			//Width and height
-			
+			var w = 400;
+			var h = 300;
+
 			//Define map projection
 
 
-			var projection = d3.geoMercator() //use a standard projection to flatten the poles, see D3 projection plugin
-								   .center([ 20, 15 ]) //how to center the map, longitude, latitude
-								   .scale(100); // bigger value, bigger zoom
+			var projection = d3.geoMercator() //utiliser une projection standard pour aplatir les pôles, voir D3 projection plugin
+								   .center([ 15, 55 ]) //comment centrer la carte, longitude, latitude
+								   .translate([ w/2, h/2 ]) // centrer l'image obtenue dans le svg
+								   .scale([ w/1.5 ]); // zoom, plus la valeur est petit plus le zoom est gros 
+
 			//Define path generator
 			var path = d3.geoPath()
-				   .projection(projection);
+					.projection(projection);
 
 
 			//Create SVG
-			var svg1 = d3.select("#child_div1")
-				  .append("svg")
-				  .attr("width", "100%")
-				  .attr("height", "90%");
+			var svg = d3.select("#child_div1")
+						.append("svg")
+						.attr("width", w)
+						.attr("height", h);
 
 			//Load in GeoJSON data
-			d3.json("https://raw.githubusercontent.com/markuslerner/travelscope/master/public/map/2.0.0/ne_50m_admin_0_countries_simplified.json", function(json) {
+			d3.json("https://raw.githubusercontent.com/markuslerner/travelscope/master/public/map/2.0.0/ne_50m_admin_0_countries_simplified.json").then (function(json) {
 				
 				//Bind data and create one path per GeoJSON feature
-				svg1.selectAll("path")
+				svg.selectAll("path")
 				   .data(json.features)
 				   .enter()
 				   .append("path")
 				   .attr("d", path)
 				   .attr("stroke", "rgba(8, 81, 156, 0.2)")
-				   .attr("fill", function(d){
-           			     if(selectedCountries.includes(d.properties.iso_a3)){
-           	                     	return "rgba(150, 81, 156, 0.6)";
-                                     }
-                                     else return "rgba(8, 81, 156, 0.6)";
-				    });
-			           
-				   
-				});
-			
+				   //.attr("fill", "rgba(8, 81, 156, 0.6)");
+          
+           .attr("fill", function(d){
+           if(selectedCountries.includes(d.properties.iso_a3)){
+           return "rgba(255, 81, 156, 0.6)";
+           }
+           else return "rgba(8, 81, 156, 0.6)";
+           })
+           .attr("id", function(d, i){
+           return "country" + d.properties.iso_a3;
+           })
+           .attr("class", "country")
+           .on("click", function(d, i){
+					d3.select("#countryname").style("display", "block").text(d.properties.name);
+					d3.select("#countrypopulation").style("display", "block").text("Population: " + d.properties.pop_est);
+					
+					if(d.properties.lastcensus == "-99"){
+						d3.select("#countrycensus").style("display", "block").text("Census data unavailable");
+					}
+					else {		  
+					d3.select("#countrycensus").style("display", "block").text("Census data from:" + d.properties.lastcensus);
+					};
+					d3.select("#countryincome").style("display", "block").text("Income bracket: " + d.properties.income_grp);
+					d3.select("#countryeconomy").style("display", "block").text("Economy: " + d.properties.economy); })
+					
+           ;  
+		
+			});
