9503ad027668baf4a2aa9c348b05e844c84a80c7
Gangascoob <67718774+Gangascoob@users.noreply.github.com>
2022-03-16 22:56:18 +0000

    Update piechart.js

--- piechart.js
+++ piechart.js
@@ -1,11 +1,11 @@
-const xSizepie  = 400;   const ySizepie  = 400; 
+const xSizepie  = 200;   const ySizepie  = 200; 
 
 
 // Append SVG Object to the Page 
 const svgpie = d3.select("#child_div3") 
               .append("svg") 
-              .attr('width',  "40%"  ) 
-              .attr('height', "60%"  ) 
+              .attr('width',  "100%"  ) 
+              .attr('height', "90%"  ) 
               .append("g") 
               .attr("transform","translate(" + xSizepie/2 + "," + ySizepie/2 + ")"); 
 const radius = Math.min(xSizepie, ySizepie) / 2; 
