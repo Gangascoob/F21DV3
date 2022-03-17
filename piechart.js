a25b74f455d06ef88a60db36b95749a38db120f4
Gangascoob <67718774+Gangascoob@users.noreply.github.com>
2022-03-17 13:37:00 +0000

    Update piechart.js

--- piechart.js
+++ piechart.js
@@ -10,21 +10,45 @@ const svgpie = d3.select("#child_div3")
               .attr("transform","translate(" + xSizepie/2 + "," + ySizepie/2 + ")"); 
 const radius = Math.min(xSizepie, ySizepie) / 2; 
 var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']); 
+let piecsv = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-age-group.csv";
 
-
-
-function piechart(name){
 	
-var data = []; //more data added
+	
+function piechart(name){
 
-d3.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations-by-age-group.csv"), function(csv){
+let piecsv = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv";
+		let data = []; //more data added
+let piedata = [];
+let filteredData = [];
+let filteredVaccNumbers = [];
+let sumVaccNumbers = 0;
+let tester = 0;
+d3.csv(piecsv, function(csv){
+data.push({location: csv.location, date: csv.date, vaccinated:
+	   + csv.people_vaccinated, 
+	   fullvaccinated: + csv.people_fully_vaccinated,
+	   booster: + csv.total_boosters});			
+}).then(function(name, date){
+filteredData = data.filter(function(d){return d.location == name && d.date == date});
+tester = filteredData;
+filteredVaccNumbers.push(filteredData.filter(function(d){return d.vaccinated}));
 
-if(name == csv.location && csv.date == "2021-07-23"){
-	data.push(csv.people_vaccinated_per_hundred);
-	console.log("yes");
-}
+console.log(data[5].location);
+console.log(filteredData[1]);
+//console.log(filteredVaccNumbers);
 });
- 
+
+	
+	
+	
+	
+	
+}	
+	
+	
+};	
+	
+function piechartcreate(data){ 
 // Generate the pie 
 var pie = d3.pie(); 
  
